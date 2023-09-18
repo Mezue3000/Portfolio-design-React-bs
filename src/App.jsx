d52ffc465ce7fc05bd4c215fc040f60f65c4aa18
mezue30000 <mezue3000>
2023-09-18 14:43:35 +0100

    created my navbar and added react-icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/Js/bootstrap.bundle.min.Js';
 import Header from "./Components/Header"
 
 function App() {
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { BsFillPieChartFill } from "react-icons/bs";
+
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
+    <Navbar expand="lg" className="bg-info py-3">
+        <Container>
+            <div>
+            <BsFillPieChartFill className='pb-1 fs-5'/>
+            <Navbar.Brand href="#home" className='fw-bolder fs-5'>Pylot's Folio</Navbar.Brand>
+            </div>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="heady fw-bolder fs-5">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#about">About</Nav.Link>
+                    <Nav.Link href="#service">Services</Nav.Link>
+                    <Nav.Link href="#work">Works</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    <Nav.Link href="#blog">Blog</Nav.Link>
+                    <Nav.Link href="#contact">Contact</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+);
 }
 
 export default Header
