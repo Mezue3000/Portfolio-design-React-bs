31ce3e8cffd4f3b7d9bbb5a8c063e550b6d01574
mezue30000 <mezue3000>
2023-09-30 09:08:28 +0100

    Added contact-form-section with Google map

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { Tooltip } from 'react-tooltip';
 import Header from "./Components/Header";
 import Home from './Components/Home';
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -7,23 +7,27 @@ const Contact = () => {
         <h1 className='about fw-bold'>Contact Us</h1>
         <Form.Group controlId="formFile" className="mb-3">
           <Form.Label className='text-light'>Name</Form.Label>
-          <Form.Control type="text" placeholder='Name' />
+          <Form.Control type="text" placeholder='Mezue Obiora' />
         </Form.Group>
         <Form.Group controlId="formFileMultiple" className="mb-3">
-          <Form.Label>Multiple files input example</Form.Label>
-          <Form.Control type="file" multiple />
+          <Form.Label className='text-light'>Email Address</Form.Label>
+          <Form.Control type="email" placeholder='Pylotworld@yahoo.com' multiple />
         </Form.Group>
         <Form.Group controlId="formFileDisabled" className="mb-3">
-          <Form.Label>Disabled file input example</Form.Label>
-          <Form.Control type="file" />
+          <Form.Label className='text-light'>Phone-number</Form.Label>
+          <Form.Control type="number" placeholder='+234...' aria-required />
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Example textarea</Form.Label>
-          <Form.Control as="textarea" rows={3} />
+          <Form.Label className='text-light'>Message</Form.Label>
+          <Form.Control as="textarea" rows={3} placeholder='Write Message' />
         </Form.Group>
+        <button className='btn btn-info' type='button'>Send Info</button>
+      </div>
+
+      <div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d253682.46316648493!2d3.118801566010113!3d6.548369255271702!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8b2ae68280c1%3A0xdc9e87a367c3d9cb!2sLagos!5e0!3m2!1sen!2sng!4v1696060299201!5m2!1sen!2sng" className='w-100 hait' allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
     </section>
   )
 }
-
 export default Contact
